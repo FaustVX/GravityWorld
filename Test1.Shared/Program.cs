f7ee22b087505e186db0694e046537e70037226b
FaustVX <jodu035@gmail.com>
2020-04-26 02:47:25 +0200

    Initial commit

---
+++ Test1.Shared/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace test1
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var game = new Game())
+            {
+                game.Run();
+            }
+        }
+    }
+}
